2f5b6c298fc24b9c0055eafdd2f7ed52b20563b5
AyjayJr <48783536+AyjayJr@users.noreply.github.com>
2023-08-15 22:53:39 -0400

    implemented binary search

--- algo/binary_search.c
+++ algo/binary_search.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 
+int binary_search(int *arr, int len, int target) {
+
+  int left = 0;
+  int right = len - 1;
+
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+
+    if (target == arr[mid]) {
+      return mid;
+    }
+    else if (target < arr[mid]) {
+      right = mid - 1; 
+    } 
+    else if (target > arr[mid]) {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+}
+
 
 int main(void) {
 
-  int arr = [1,2,3,4,5];
-  printf("%d", arr); 
+  int len = 5;
+  int arr[] = {1,2,3,4,5};
+  
+  for (int i = 0; i < len; i++) {
+    printf("Element %d: %d\n", i, arr[i]);
+  }
+
+  int index = binary_search(arr, len, 2);
+
+  if (index != -1) {
+    printf("Element found at index %d\n", index);
+  }
+  else {
+    printf("Element not found\n");
+  }
+
   return 0;
 }
